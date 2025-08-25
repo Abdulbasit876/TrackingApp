abd3302aac6605c570c35a21182f3c8bcc824423
Abdulbasit876 <codesoftz1@gmail.com>
2025-08-25 13:03:19 +0500

    W

--- Clients/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
-
-const config = getDefaultConfig(__dirname);
-config.resolver.extraNodeModules = {
-  tslib: require.resolve("tslib"),
-};
-
-module.exports = withNativeWind(config, { input: './global.css' });
